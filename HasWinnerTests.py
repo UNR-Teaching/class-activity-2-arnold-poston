72e1dc9a62c02eeb0d7e5f5b1d670ec78b649437
nmarno68 <narnold@nevada.unr.edu>
2020-01-28 16:44:46 -0800

    Basic board testing for full boards

--- HasWinnerTests.py
+++ HasWinnerTests.py
@@ -1,9 +1,34 @@
 import unittest
+from tictactoe import Board
 
 
 class HasWinnerTests(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.tictactoe = Board()
+        self.staleMateBoard = [['X', 'O', 'X'],
+                               ['O', 'X', 'O'],
+                               ['O', 'X', 'O']]
+
+        self.winnerRowBoard = [['O', 'X', 'O'],
+                                 ['X', 'X', 'X'],
+                                 ['O', 'X', 'X']]
+
+        self.winnerColBoard = [['X', 'O', 'X'],
+                               ['O', 'O', 'X'],
+                               ['X', 'O', 'O']]
+
+
+    def test_stalemate(self):
+        self.tictactoe.board = self.staleMateBoard
+        self.assertFalse(self.tictactoe.has_winner())
+
+    def test_RowWinner(self):
+        self.tictactoe.board = self.winnerRowBoard
+        self.assertTrue(self.tictactoe.has_winner())
+
+    def test_ColWinner(self):
+        self.tictactoe.board = self.winnerColBoard
+        self.assertTrue(self.tictactoe.has_winner())
 
 
 if __name__ == '__main__':
