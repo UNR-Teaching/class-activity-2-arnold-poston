86e40a1ca6ad1b1c450fa8f1f5176dbf1f3f9ec5
nmarno68 <narnold@nevada.unr.edu>
2020-02-11 15:40:57 -0800

    separated board and game driver

---
+++ GameDriver.py
@@ -0,0 +1,33 @@
+
+from Board import Board
+
+
+class GameDriver(object):
+
+    def __init__(self):
+        self.board = Board()
+        pass
+
+    def play_game(self):
+        """
+        Takes moves from raw_input as comma-separated list in form (column, row, player)
+            and makes a move. When a winner has been determined, the game ends
+
+        :return: (str) the letter representing the player who won
+        """
+
+
+        self.board.print()
+        while not self.board.has_winner() and not self.board.check_full_board():
+            user_entered_data = input("Enter in a move in the form (column, row, player)")
+            user_entered_data = user_entered_data.replace(' ', '')
+            column, row, player = user_entered_data.split(',')
+
+            # Can break if column or row is not int
+            column = int(column)
+            row = int(row)
+
+            self.board.mark_square(column, row, player)
+            self.board.print()
+        return self.board.has_winner()
+        pass
