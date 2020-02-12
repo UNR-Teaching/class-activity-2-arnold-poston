819dd2bea519d83802b8e13e8737928ebc0c73d1
Price <pricep@nevada.unr.edu>
2020-02-11 16:01:46 -0800

    Made regex for player getmove

--- Player.py
+++ Player.py
@@ -1,4 +1,5 @@
 from itertools import count
+import re
 
 
 class Player(object):
@@ -10,25 +11,29 @@ class Player(object):
         self.playerNumber = next(self._ids)
 
     def getMove(self):
-        user_entered_data = input("Player {} enter in a move in the form (column, row)".format(self.playerNumber))
-        user_entered_data = user_entered_data.replace(' ', '')
-        column, row = user_entered_data.split(',')
-
-        # Can break if column or row is not int
-        column = int(column)
-        row = int(row)
-
-        while not self.check_valid_row_column(column, row):
-            print("You have entered an invalid move, please try again.")
-            user_entered_data = input("Player {} enter in a move in the form (column, row)".format(self.playerNumber))
+        correctInputPattern = re.compile("\s*-?[0-9]\s*,\s*-?[0-9]\s*$")
+        user_entered_data = input("Player {} enter in a move in the form (row, column)".format(self.playerNumber))
+        column = -1
+        row = -1
+        if correctInputPattern.match(user_entered_data):
             user_entered_data = user_entered_data.replace(' ', '')
-            column, row = user_entered_data.split(',')
+            row, column = user_entered_data.split(',')
 
             # Can break if column or row is not int
             column = int(column)
             row = int(row)
 
-        return column, row
+        while not self.check_valid_row_column(column, row):
+            print("You have entered an invalid move, please try again.")
+            user_entered_data = input("Player {} enter in a move in the form (row, column)".format(self.playerNumber))
+            if correctInputPattern.match(user_entered_data):
+                user_entered_data = user_entered_data.replace(' ', '')
+                row, column = user_entered_data.split(',')
+
+                # Can break if column or row is not int
+                column = int(column)
+                row = int(row)
+        return row, column
 
     def check_valid_row_column(self, column, row):
         if column < 0 or column > 2:
@@ -36,3 +41,6 @@ class Player(object):
         if row < 0 or row > 2:
             return False
         return True
+
+player = Player("s")
+player.getMove()
